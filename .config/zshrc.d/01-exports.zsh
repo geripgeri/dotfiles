305d9740b281766e1b55a0c4525cbb26d04a38ec
Gergely Peidl <gergely@peidl.net>
2022-01-06 22:17:18 +0100

    Update exports.zsh

--- .config/zshrc.d/01-exports.zsh
+++ .config/zshrc.d/01-exports.zsh
@@ -1,3 +1,4 @@
 export EDITOR=nvim
 export SSH_AUTH_SOCK=/run/user/$(id -u)/ssh-agent.socket
-export excluded_dirs=.git,.python-packages,.gem
+export excluded_dirs="--exclude-dir={.git,.svn,.tox,.python-packages,.gem}"
+export VIRTUAL_ENV_DISABLE_PROMPT=0
